21aa8bef7f9a3d0800ed03422d5c1af53d32a7c5
ayman <aemanharde@gmail.log>
2026-02-01 22:25:59 +0300

    setting header component

--- src/Customer-View.jsx
+++ src/Customer-View.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <p>Smart Stadium</p>;
+  return <Header />;
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="title">6 a Side Stadium</h1>
+      <p className="sub-title">Football is Life, Enjoy Your Life!</p>
+    </div>
+  );
 }
