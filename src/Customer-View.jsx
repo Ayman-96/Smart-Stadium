21c6e0aa0db5b5caf86e8a0c98e44901cf3785a7
ayman <aemanharde@gmail.log>
2026-02-01 22:56:42 +0300

    Add Footer

--- src/Customer-View.jsx
+++ src/Customer-View.jsx
@@ -20,6 +20,7 @@ export default function App() {
     <div>
       <Header />
       <StadiumsView />
+      <Footer />
     </div>
   );
 }
@@ -52,11 +53,21 @@ function StadiumsView() {
 
 function StadSlot({ name, price, img }) {
   return (
-    <div>
+    <div className="slots">
       <p className="stad-name">Stadium {name}</p>
       <img src={img} className="stad-img"></img>
       <div className="stad-price">Price : {price} per Ticket</div>
       <button className="show-button">See Details</button>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div id="left-bottom">Designed By : Ayman</div>
+      <div id="center-bottom">Smart Stadium</div>
+      <div id="right-bottom">example@gmail.com</div>
     </div>
   );
 }
